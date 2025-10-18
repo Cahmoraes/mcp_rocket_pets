749826d58b5ea0daaa2cdb38ade024868e289226
Caique Moraes <ike.moraes93@gmail.com>
2025-10-18 08:44:00 -0300

    add init

---
+++ init/schema.sql
@@ -0,0 +1,24 @@
+-- Active: 1760787463477@@127.0.0.1@3306
+CREATE TABLE IF NOT EXISTS 'pets' (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS 'people' (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    pet_id INTEGER NOT NULL,
+    FOREIGN KEY (pet_id) REFERENCES pets (id)
+);
+
+INSERT INTO
+    pets (name, type)
+values ("cobra", "snake"),
+    ("cao", "dog"),
+    ("gato", "cat"),
+    ("jorgin", "hamster"),
+    ("burro", "donkey"),
+    ("shrek", "ogro"),
+    ("belinha", "dog")
